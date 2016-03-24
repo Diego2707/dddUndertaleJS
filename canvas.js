77a4f22bfe81280e31fe3619008be024109493bd
J0SLAN <J0SLAN@users.noreply.github.com>
2016-03-24 14:40:31 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,25 +2,29 @@
 
 ////////////////////////Keyboard input
 
-var prKeys=[],
-    recent=[];
-Window.onkeydown=function(e){
-  prKeys[recent[recent.push(e.keyCode)-1]]=true;
+var prKeys={};
+window.onkeydown=function(e){
+  prKeys[e.keyCode]=true;
+  return false;
 }
-Window.onkeyup=function(e){
-  delete prKeys[recent.pop()];
+window.onkeyup=function(e){
+  prKeys[e.keyCode]=undefined;
 }
-Window.onblur=function(){
-  prKeys=[],recent=[];
+window.onblur=function(){
+  prKeys=[];
 }
 
 ////////////////////////Important shit
 
 var canvas=document.getElementById('canvas'),
     ctx=canvas.getContext('2d'),
-    heartX=0,heartY=0,
+    heartSpr=new Image(),
     sprite={},onSprites=[],
+    heartX=0,heartY=0,
     HP=20,items=[];
+
+heartSpr.src="https://static.tumblr.com/f6c48b514aa41da79dd0eb3d931019fb/ycfppqo/81no29htn/tumblr_static_9xc2tj68dcwg4w8gw4so0csco.png"
+
 
 sprite["pellet"]={
   onHit:function(){
@@ -45,7 +49,9 @@ sprite["pellet"]={
 
 sprite["misterAsshole"]={
   onHit:function(){},
-  onInit:function(){},
+  onInit:function(){
+  	return true;
+  },
   onTick:function(){},
   onTurn:function(){},
   //imgs:[<images>] //Seriously how the fuck do I do this
@@ -58,9 +64,9 @@ onSprites=[
 
 
 function tick(time){
-  var heartSpd=1
+  var heartSpd=2
   if(prKeys[88]||prKeys[16]){
-    var heartSpd=.5
+    var heartSpd=1
   }if(prKeys[37]){
     heartX-=heartSpd;
   }if(prKeys[38]){
@@ -70,17 +76,17 @@ function tick(time){
   }if(prKeys[40]){
     heartY+=heartSpd;
   }
+  ctx.clearRect(0,0,canvas.width,canvas.height)
   for(let i=0;i<onSprites.length;i++){
     let spr=onSprites[i];
     if(spr[7]==undefined){
       spr[7]=sprite[spr[0]].onInit(spr);
     }
     sprite[spr[0]].onTick(spr[7]);
-    
     //Draw sprite from spr[5]
   }
   
-  //Draw heart
+  ctx.drawImage(heartSpr,heartX,heartY,20,20)
   
   window.requestAnimationFrame(tick);
 }
